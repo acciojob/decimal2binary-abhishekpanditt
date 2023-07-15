884f978c03e21aa52b519bdcb669b7d7335481af
abhishekpanditt <123313123+abhishekpanditt@users.noreply.github.com>
2023-07-15 16:36:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function decimalToBinary(num) {
   //Write you code here
-  
+  if (num === 0) {
+    return '0'; // Special case for 0
+  }
+
+  var binary = '';
+  while (num > 0) {
+    binary = (num % 2) + binary; // Get the remainder (0 or 1) and prepend it to the binary string
+    decimal = Math.floor(num / 2); // Divide the decimal by 2 to move to the next bit
+  }
+
+  return binary;
 }
+
 
 window.decimalToBinary = decimalToBinary;
